f248f9534e7a0abc4d44765b344b2b9738422a24
m.forlivesi <m.forlivesi@reply.it>
2022-03-06 03:25:19 +0100

    calls

--- Assets/Hackathon/Scripts/ApplicationController.cs
+++ Assets/Hackathon/Scripts/ApplicationController.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public enum UserType
 {
@@ -17,7 +20,29 @@ public class ApplicationController : MonoBehaviour
     [SerializeField] private Transform userChoiceCanvas;
     [SerializeField] private Transform houseSelectionCanvas;
     [SerializeField] private Transform errorPopup;
+    [SerializeField] private Image documentImage;
+    [SerializeField] private Button showDocument;
+    [Header("Buyer")]
+    [SerializeField] private Transform buyerCanvas;
+    [SerializeField] private Button startProposal;
+    [Header("Owner")]
+    [SerializeField] private Transform ownerCanvas;
+    [SerializeField] private Button acceptProposal;
+    [SerializeField] private Button denyProposal;
+    [Header("Request")]
+    [SerializeField] private Transform waitCanvas;
+    [SerializeField] private Transform resultCanvas;
+    [SerializeField] private TMP_Text confirmText;
     private int userType;
+
+
+    private string showDocumentUri = "https://metaverse-rest-api.herokuapp.com/getDocuments/0";
+    private string isAuthorizedUri = "https://metaverse-rest-api.herokuapp.com/isAuthorized/{type}";
+    private string startProposalUri = "https://metaverse-rest-api.herokuapp.com/startProposal";
+    private string showProposalUri = "https://metaverse-rest-api.herokuapp.com/showProposal";
+    private string declineProposalUri = "https://metaverse-rest-api.herokuapp.com/declineProposal";
+    private string confirmProposalUri = "https://metaverse-rest-api.herokuapp.com/confirmProposal";
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +52,8 @@ public class ApplicationController : MonoBehaviour
         userChoiceCanvas.gameObject.SetActive(true);
         houseSelectionCanvas.gameObject.SetActive(false);
         errorPopup.gameObject.SetActive(false);
+        waitCanvas.gameObject.SetActive(false);
+        resultCanvas.gameObject.SetActive(false);
         //DontDestroyOnLoad(gameObject);
     }
 
@@ -44,6 +71,17 @@ public class ApplicationController : MonoBehaviour
             //SceneManager.LoadScene("Complete");
             houseScene.gameObject.SetActive(true);
             initScene.gameObject.SetActive(false);
+
+            if (userType == 0)
+            {
+                ownerCanvas.gameObject.SetActive(true);
+                buyerCanvas.gameObject.SetActive(false);
+            }
+            else
+            {
+                ownerCanvas.gameObject.SetActive(false);
+                buyerCanvas.gameObject.SetActive(true);
+            }
         }
         else
         {
@@ -56,5 +94,67 @@ public class ApplicationController : MonoBehaviour
         userChoiceCanvas.gameObject.SetActive(true);
         houseSelectionCanvas.gameObject.SetActive(false);
         errorPopup.gameObject.SetActive(false);
+    }
+
+    public void StartProposalRequest()
+    {
+        StartCoroutine(StartRequestCoroutine(startProposalUri, OnStartProposalEnd));
+    }
+
+    private void OnStartProposalEnd(string result)
+    {
+        confirmText.text = "Proposal sent: " + result;
+    }
+
+    public void ShowProposalRequest()
+    {
+        StartCoroutine(StartRequestCoroutine(showProposalUri, OnShowProposalEnd));
+    }
+
+    private void OnShowProposalEnd(string result)
+    {
+        confirmText.text = "Proposal sent: " + result;
+    }
+
+    public void AcceptProposalRequest()
+    {
+        StartCoroutine(StartRequestCoroutine(confirmProposalUri, OnAcceptProposalEnd));
+    }
+
+    private void OnAcceptProposalEnd(string result)
+    {
+        confirmText.text = "Proposal accepted: " + result;
+    }
+
+    public void DeclineProposalRequest()
+    {
+        StartCoroutine(StartRequestCoroutine(declineProposalUri, OnDeclineProposalEnd));
+    }
+
+    private void OnDeclineProposalEnd(string result)
+    {
+        confirmText.text = "Proposal denied: " + result;
+    }
+
+
+    public IEnumerator StartRequestCoroutine(string uri, System.Action<string> onComplete)
+    {
+        waitCanvas.gameObject.SetActive(true);
+        UnityWebRequest uwr = UnityWebRequest.Get(uri);
+        yield return uwr.SendWebRequest();
+        var result = "";
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            result = uwr.downloadHandler.text;
+            Debug.Log("Image uri: " + result);
+        }
+        waitCanvas.gameObject.SetActive(false);
+        resultCanvas.gameObject.SetActive(true);
+        onComplete(result);
     }
 }
---
+++ Assets/Hackathon/Scripts/NFTCall.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class NFTCall : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Button showDocument;
+    private Sprite mySprite;
+    // [SerializeField] private Texture2D myTexture2D;
+    // Start is called before the first frame update
+    public void ShowDocument()
+    {
+        StartCoroutine(getRequest("https://metaverse-rest-api.herokuapp.com/getDocuments/0"));
+        showDocument.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    IEnumerator getRequest(string uri)
+    {
+        UnityWebRequest uwr = UnityWebRequest.Get(uri);
+        yield return uwr.SendWebRequest();
+        var image_uri = "";
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            image_uri = uwr.downloadHandler.text.Substring(2, uwr.downloadHandler.text.Length - 4);
+            Debug.Log("Image uri: " + image_uri);
+        }
+        UnityWebRequest uwr2 = UnityWebRequestTexture.GetTexture(image_uri);
+        yield return uwr2.SendWebRequest();
+        if (uwr2.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr2.error);
+        }
+        else
+        {
+            var myTexture = ((DownloadHandlerTexture)uwr2.downloadHandler).texture;
+            mySprite = Sprite.Create(myTexture, new Rect(0.0f, 0.0f, myTexture.width, myTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+            image.sprite = mySprite;
+            image.gameObject.SetActive(true);
+        }
+
+    }
+}
